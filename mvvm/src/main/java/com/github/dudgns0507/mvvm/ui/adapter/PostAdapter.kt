3a85f75bd160a72fa23606e164bd0e37d12e9e60
copin-danny <hoon2585@gmail.com>
2021-10-08 14:40:22 +0900

    Update PostAdapter.kt

--- mvvm/src/main/java/com/github/dudgns0507/mvvm/ui/adapter/PostAdapter.kt
+++ mvvm/src/main/java/com/github/dudgns0507/mvvm/ui/adapter/PostAdapter.kt
@@ -10,7 +10,32 @@ import com.github.dudgns0507.domain.dto.Post
 import com.github.dudgns0507.mvvm.databinding.PostItemBinding
 import com.github.dudgns0507.mvvm.ui.holder.PostViewHolder
 
-class PostAdapter : BaseAdapter<Post, PostViewHolder>() {
+/**
+ * BaseAdapter
+ *
+ * Default RecyclerView adapter.
+ *
+ * getItem(position: Int) - get item with position
+ * updateList(list: List<T>) - change adapter list to parameter list
+ * addList(list: List<T>) - append parameter list to adapter list
+ * clearList() - clear adapter list
+ *
+ *
+ * BaseDiffAdapter
+ *
+ * Use DiffUtil to increase loading performance.
+ * Refresh only changed data.
+ *
+ * Default method is same with BaseAdapter.
+ *
+ * isNewItem(oldItem: T, newItem: T): Boolean
+ * isNewContent(oldItem: T, newItem: T): Boolean
+ *
+ * You should override two method for using DiffUtil
+ *
+ */
+
+class PostAdapter : BaseDiffAdapter<Post, PostViewHolder>() {
     lateinit var onItemClickListener: OnItemClickListener<Post>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
@@ -26,5 +51,13 @@ class PostAdapter : BaseAdapter<Post, PostViewHolder>() {
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
         holder.bind(position, getItem(position))
+    }
+
+    override fun isNewItem(oldItem: Post, newItem: Post): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun isNewContent(oldItem: Post, newItem: Post): Boolean {
+        return oldItem == newItem
     }
 }
